73be876000629acab115bee2f9f9743c8362e495
swethaogeti <65771591+swethaogeti@users.noreply.github.com>
2023-01-13 15:14:39 +0530

    style : css styling in product card

--- src/frontend/components/productCard/ProductCard.js
+++ src/frontend/components/productCard/ProductCard.js
@@ -4,26 +4,24 @@ import "./productCard.css";
 export const ProductCard = () => {
   return (
     <div className="productCard">
-      <div className="productCard__img">
-        <img src="https://m.media-amazon.com/images/I/61LsaxVQ4iL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg"></img>
-      </div>
+      <img src="https://edit.org/photos/editor/json/2021/09/30/a/2/a20ea9796751090ea0dba134de3d22e0_edit.org.jpg-376.jpg"></img>
 
       <div className="productCard__details">
-        <div>
-          <h1>It end with us</h1>
+        <div className="productCard__details__desp">
+          <h2>It end with us</h2>
           <p>by xyz kate</p>
         </div>
-        <div>
+        <div className="productCard__details__price">
           <h2>₹2999</h2>
           <span>₹3999</span>
           <h3>25% Off</h3>
         </div>
-        <div>
-          <button>
-            <ShoppingCartRounded />
-            <h3>Go to Cart</h3>
-          </button>
-        </div>
+        {/* <div> */}
+        <button>
+          <ShoppingCartRounded />
+          <h3>Go to Cart</h3>
+        </button>
+        {/* </div> */}
       </div>
     </div>
   );
--- src/frontend/pages/productListingPage/ProductListingPage.js
+++ src/frontend/pages/productListingPage/ProductListingPage.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { ProductCard } from "../../components/productCard/ProductCard";
-
+import "./productListingPage.css";
 export const ProductListingPage = () => {
   return (
-    <div>
+    <div className="productlisting">
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
       <ProductCard />
     </div>
   );
