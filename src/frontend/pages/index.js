79ab9cbf7f86611ce21ffbe84d568e98c09a0879
swethaogeti <65771591+swethaogeti@users.noreply.github.com>
2023-01-14 15:25:12 +0530

    fix: importing files

--- src/frontend/pages/index.js
+++ src/frontend/pages/index.js
@@ -1,4 +1,4 @@
-export { SignUpPage } from "./signUpPage/SignUpPage";
+export { SignUpPage } from "./signupPage/SignUpPage";
 export { LoginPage } from "./loginPage/LoginPage";
 export { ProductListingPage } from "./productListingPage/ProductListingPage";
 export { WishlistPage } from "./wishlistPage/WishlistPage";
