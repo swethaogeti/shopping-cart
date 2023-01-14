4b5dd25c05d68f7c870b47ff5b2bbcd6d14b3ffe
swethaogeti <65771591+swethaogeti@users.noreply.github.com>
2023-01-14 13:30:55 +0530

    feat : added login page

--- src/frontend/pages/loginPage/LoginPage.js
+++ src/frontend/pages/loginPage/LoginPage.js
@@ -1,5 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="formPage">
+      <form className="formPage__container">
+        <h1>login</h1>
+
+        <label for="email">Email*</label>
+        <input
+          type="email"
+          id="email"
+          placeholder="sweety123@gmail.com"
+        ></input>
+
+        <label for="password">Password*</label>
+        <input type="password" id="password"></input>
+
+        <button>Login</button>
+        <button>Login as a Guest</button>
+        <p>
+          New here?{" "}
+          <Link to="/signup" className="formPage__container__link">
+            create an account!
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
 };
