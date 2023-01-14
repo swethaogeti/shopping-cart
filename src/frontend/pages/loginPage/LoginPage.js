77bbdd5133c304b52710a44b75a5326d8ce0911e
swethaogeti <65771591+swethaogeti@users.noreply.github.com>
2023-01-14 14:04:04 +0530

    refactor : css in  formPage

--- src/frontend/pages/loginPage/LoginPage.js
+++ src/frontend/pages/loginPage/LoginPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "../signUpPage/formPage.css";
+import "./loginPage.css";
 export const LoginPage = () => {
   return (
     <div className="formPage">
@@ -18,7 +19,7 @@ export const LoginPage = () => {
         <input type="password" id="password"></input>
 
         <button>Login</button>
-        <button>Login as a Guest</button>
+        <button className="login">Login as a Guest</button>
         <p>
           New here?{" "}
           <Link to="/signup" className="formPage__container__link">
